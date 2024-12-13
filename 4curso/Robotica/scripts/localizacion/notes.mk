b62b52cf54b8d195c96820866caf23e6fbd154b8
ANCOR <alu0101327679@cc1316.etsii.ull.es>
2024-12-13 12:43:00 +0000

    localizacion start

---
+++ 4curso/Robotica/scripts/localizacion/notes.mk
@@ -0,0 +1,9 @@
+linea roja es el movimiento de verdad
+la verde es el idea
+la discontinua es la inea de union entre los puntos
+el sensor da las distancias a las balizas 
+si los valores de los robots difieren se llama a la funcion de localizacion
+
+
+Mapa de calor
+las más frías son dinde está el robot
